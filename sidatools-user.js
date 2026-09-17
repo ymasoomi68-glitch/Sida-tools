c9c18c018cbf85b6aeb0c4f78728e7678dcbe2aa
ymasoomi68-glitch <ymasoomi68@gmail.com>
2026-09-17 09:10:10 +0330

    Update sidatools-user.js

--- sidatools-user.js
+++ sidatools-user.js
@@ -5,8 +5,8 @@
 // @description  داشبورد کشویی ابزارهای کمکی سیدا - نسخه قفل‌دار
 // @author       You
 // @match        https://sida.medu.ir/*
-// @updateURL    https://raw.githubusercontent.com/ymasoomi68-glitch/Sida-tools/main/sidatools-user.js
-// @downloadURL  https://raw.githubusercontent.com/ymasoomi68-glitch/Sida-tools/main/sidatools-user.js
+// @updateURL    https://cdn.jsdelivr.net/gh/ymasoomi68-glitch/Sida-tools@main/sidatools-user.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/ymasoomi68-glitch/Sida-tools@main/sidatools-user.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @require      https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js
