5aac7fc4f6b215b886668c3ca7c96b8263ddf0f1
ymasoomi68-glitch <ymasoomi68@gmail.com>
2026-09-17 09:57:45 +0330

    Update sidatools-user.js

--- sidatools-user.js
+++ sidatools-user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         🛠️ ابزارهای سیدا
+// @name         🛠️ ابزارهای هوشمند سیدا
 // @namespace    http://tampermonkey.net/
-// @version      15.8
+// @version      15.9
 // @description  داشبورد کشویی ابزارهای کمکی سیدا - نسخه قفل‌دار
 // @author       You
 // @match        https://sida.medu.ir/*
