a2aff059c442dcbf0a59ed56f26b9f7b4044f742
ymasoomi68-glitch <ymasoomi68@gmail.com>
2026-09-17 20:00:20 +0330

    Update binatosida-mobile.user.js

--- binatosida-mobile.user.js
+++ binatosida-mobile.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         📱 انتقال از بینا به سیدا (نسخه موبایل)
 // @namespace    http://tampermonkey.net/
-// @version      3.3-mobile
+// @version      1.0-mobile
 // @description  جمع‌آوری خودکار همه دانش‌آموزان از بینا - نسخه موبایل
 // @author       یوسف معصومی
 // @match        https://bina.medu.ir/*
